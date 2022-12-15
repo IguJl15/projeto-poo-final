41df236db6e1d1d7ee85db6c3227f2bc9292c414
Igor Julliano <igor.julliano2@gmail.com>
2022-12-15 11:22:32 -0300

    Remove files that doesn't need to be tracked

--- build/common/errors/ApplicationError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ApplicationError {
-    constructor(message, details) {
-        this.message = message;
-        this.details = details;
-    }
-}
-exports.default = ApplicationError;
